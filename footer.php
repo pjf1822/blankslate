176769e19e00e3e84f8235cbe03618604a50d7bc
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-17 17:53:35 -0500

    ok yeah

--- footer.php
+++ footer.php
@@ -2,11 +2,32 @@
 <?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div id="copyright">
+        &copy; <?php echo esc_html(date_i18n(__('Y', 'blankslate'))); ?>
+        <?php echo esc_html(get_bloginfo('name')); ?>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector(".menu-toggle");
+        const menuContainer = document.querySelector("#menu");
+        const closeButton = document.querySelector(".menu-close");
+
+
+        menuToggle.addEventListener("click", function () {
+            menuContainer.classList.toggle("open");
+            const isExpanded =
+                menuToggle.getAttribute("aria-expanded") === "true" || false;
+            menuToggle.setAttribute("aria-expanded", !isExpanded);
+        });
+        closeButton.addEventListener("click", function () {
+            menuContainer.classList.remove("open");
+            menuToggle.setAttribute("aria-expanded", "false");
+        });
+    });
+</script>
 </body>
+
 </html>
